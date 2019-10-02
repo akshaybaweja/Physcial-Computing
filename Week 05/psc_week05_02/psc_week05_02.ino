5e61626cd79a133fe747d9248c7e250041abcc35
Akshay Baweja <akshaybaweja.1996@gmail.com>
2019-10-02 19:40:27 -0400

    code update week 05 | 02

--- Week 05/psc_week05_02/psc_week05_02.ino
+++ Week 05/psc_week05_02/psc_week05_02.ino
@@ -1,15 +1,10 @@
 #define LED_PIN 3
 #define SENSOR A0
 
-unsigned int current_time;
-float ease_time, ledValue;
-boolean pauseCode;
+boolean flag;
 
 void setup() {
-  ease_time = 2500.0;
-  current_time = 0;
-  ledValue = 0;
-  pauseCode = false;
+  flag = false;
 
   pinMode(LED_PIN, OUTPUT);
 
@@ -17,25 +12,18 @@ void setup() {
 }
 
 void loop() {
-  current_time = millis();
-  ledValue = sin(2 * PI / ease_time * current_time);
-  
+
   if (analogRead(SENSOR) > 600) {
     digitalWrite(LED_PIN, LOW);
-    pauseCode = false;
+    delay(500);
+    flag = true;
   }
 
-  if (!pauseCode && ledValue > 0.0) {
-    Serial.println(ledValue);
-
-    if (ledValue > 0.0) {
-      ledValue = 255 * ledValue;
-      analogWrite(LED_PIN, int(ledValue));
-      Serial.println("\tDONE!");
+  if (flag){
+    for (float i = 0.00; i < 1.57; i += 0.05) {
+      analogWrite(LED_PIN, int(255 * sin(i)));
+      delay(100);
     }
-
-    if (int(round(ledValue * 10) / 10) == 255) {
-      pauseCode = true;
-    }
+    flag = false;
   }
 }
